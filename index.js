69c564d961ca7f0412a3955a27a3a3a85187865d
Mart Haamer <marthaamer123@gmail.com>
2024-11-29 13:42:21 +0200

    Added API fetching Added error displaying Made modifications for design

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
 const express = require('express');
 const path = require('path');
-
+const fetch = require('node-fetch');
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-app.get('/', (req, res) => {
+require('dotenv').config();
+const apiKey = process.env.WEATHER_API_KEY;
 
+async function getWeatherData(cityName) {
+    cityName = cityName.toLowerCase()
 
-  res.render('index');
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},EE&appid=${apiKey}&units=metric`;
+        const response = await fetch(url);
+    
+        // Check if the response was successful (status code 200)
+        if (!response.ok) {
+          throw new Error(`Error fetching weather data: ${response.statusText}`);
+        }
+    
+        // Parse the response as JSON
+        const data = await response.json();
+    
+        // Extract and log relevant weather data
+        const { main, weather, name } = data;
+        console.log(`Weather in`, weather);
+        
+        const weatherData = {
+            city: name,
+            temp: main.temp,
+            type: weather[0].main,
+            icon: weather[0].icon
+        }
+        
+        return weatherData
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+};
+
+app.get('/', async (req, res) => {
+    const weatherData = await getWeatherData('Tartu')
+    console.log(weatherData)
+    res.render('index', {weatherData});
+});
+
+app.get('/requestWeather', async (req, res) => {
+    const cityName = req.query.city;
+
+    if (cityName === '') return;
+
+    const weatherData = await getWeatherData(cityName)
+    if (weatherData === undefined) return res.status(500).json({ error: 'Ei saanud ilma infot!' })
+        
+    res.json(weatherData);
 });
 
 
